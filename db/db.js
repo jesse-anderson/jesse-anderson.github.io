3079bb64cd6d58ac52aa477fc095b61f0b158d45
jesse-anderson <75538757+jesse-anderson@users.noreply.github.com>
2023-05-18 18:35:10 -0500

    Update db.js
    edit text for mobile

--- db/db.js
+++ db/db.js
@@ -72,7 +72,7 @@ export const projects = {
       projectName: "Biochemical Plant Simulation",
       image: "images/seniorDesign.jpg",
       summary:
-        "Simulated a l. reuteri 1,3-propanediol plant producing ~60,000 tons/year of product with 99% purity and generating a net profit. Simulation done in SUPERPRO and distillation columns in ASPEN PLUS. Project won 1st Place in the Engineering Expo at UIC 2023, Chemical Engineering Division.",
+        "Simulated a l. reuteri 1,3-propanediol plant producing ~60,000 tons/year. Project won 1st Place in the Engineering Expo at UIC 2023, ChE Division.",
       preview: "files/seniorDesign.pdf",
       techStack: ["Aspen Plus","SuperPro Designer", "Chemical Engineering"],
     },
