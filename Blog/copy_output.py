73778327630dd4db39b826c8333b41be30a49ad6
Jesse Anderson <75538757+jesse-anderson@users.noreply.github.com>
2024-09-03 11:19:31 -0500

    typo

--- Blog/copy_output.py
+++ Blog/copy_output.py
@@ -5,12 +5,22 @@ import os
 source_dir = os.path.join(os.getcwd(), "_site")
 print(source_dir)
 
-# Define the destination directory (one level up in the GitHub folder)
+# Define the destination directory (level up in the GitHub folder)
 destination_dir = os.path.abspath(os.path.join(os.getcwd(), "../../Blog"))
 print(destination_dir)
 
-# # Remove the destination directory if it exists and copy the contents
-if os.path.exists(destination_dir):
-    shutil.rmtree(destination_dir)
+# Ensure the destination directory exists
+if not os.path.exists(destination_dir):
+    os.makedirs(destination_dir)
 
-shutil.copytree(source_dir, destination_dir)
+# Copy the contents of the source directory to the destination directory
+for item in os.listdir(source_dir):
+    source_item = os.path.join(source_dir, item)
+    destination_item = os.path.join(destination_dir, item)
+    
+    if os.path.isdir(source_item):
+        # If the item is a directory, copy it recursively
+        shutil.copytree(source_item, destination_item, dirs_exist_ok=True)
+    else:
+        # If the item is a file, copy it
+        shutil.copy2(source_item, destination_item)
